aa6dcf5dd00f850532ebe9ef9d07f6881129f681
ThiraphatK <thiraphat_khar@cmu.ac.th>
2024-02-24 22:25:25 +0700

    part-4

--- main.js
+++ main.js
@@ -4,8 +4,10 @@ createApp({
     setup() {
         const product = ref('Boots');
         const description = ref('A pair of warm, fuzzy boots');
+        const image = ref('./assets/image/socks_green.jpg')
+        const linkCamtCmu = ref('https://www.camt.cmu.ac.th/')
         return {
-            product, description
+            product, description, image, linkCamtCmu
         }
     }
 }).mount('#app');
