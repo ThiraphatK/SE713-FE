0ad8cf9265d0b8687bbc7936ead21793b8fe4d84
ThiraphatK <thiraphat_khar@cmu.ac.th>
2024-02-25 13:47:25 +0700

    part-7

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ createApp({
         const description = ref('A pair of warm, fuzzy boots');
         const image = ref('./assets/image/socks_green.jpg');
         const linkCamtCmu = ref('https://www.camt.cmu.ac.th/');
-        const inStock = ref(true);
+        const inStock = ref(false);
         const inventory = ref(9);
         const onSale = ref(true);
         const details= ref([
@@ -15,8 +15,8 @@ createApp({
             '20% polyester'
         ]);
         const variant = ref([
-            {id: 2234, color: 'green'},
-            {id: 2235, color: 'blue'}
+            {id: 2234, color: 'green', image: './assets/image/socks_green.jpg'},
+            {id: 2235, color: 'blue', image: './assets/image/socks_blue.jpg'}
         ]);
 
         const sizes = ref([
@@ -25,6 +25,19 @@ createApp({
             {id: 3, size: 'L'}
         ]);
 
+        const cart = ref(0);
+
+        function addToCart() {
+            cart.value += 1;
+        }
+
+        function updateImage(variantImage) {
+            image.value = variantImage;
+        }
+
+        function updateInStock() {
+            inStock.value = ref(true);
+        }
         return {
             product, 
             description, 
@@ -35,7 +48,11 @@ createApp({
             onSale,
             details,
             variant,
-            sizes
+            sizes,
+            cart,
+            addToCart,
+            updateImage,
+            updateInStock
         }
     }
 }).mount('#app');
